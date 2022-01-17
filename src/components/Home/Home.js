f9bc4001947f6da2a7a2cfa977b699486cf27348
dspirovski <dspirovski@hotmail.com>
2022-01-17 19:39:50 +0100

    style updating

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,29 +17,41 @@ function Home({ setHumanChoise }) {
         </div>
       </div>
       <div className="home">
+        {/* Easy mode */}
         {gameMode === true ?
-          <main className="flex-container">
+          <main className="easy-mode-container">
             <div className="top">
-              <div className="top-left" onClick={() => { setHumanChoise('rock') }}>
-                <div className="parent-rock">
-                  <div className="child child-rock"></div>
-                </div>
-              </div>
-              <div className="top-right" onClick={() => { setHumanChoise('paper') }}>
+              <div className="top-left" onClick={() => { setHumanChoise('paper') }}>
                 <div className="parent-paper">
                   <div className="child child-paper"></div>
                 </div>
               </div>
-            </div>
-            <div className="bottom">
-              <div className="bottom-center" onClick={() => { setHumanChoise('scissors') }}>
+              <div className="top-right" onClick={() => { setHumanChoise('scissors') }}>
                 <div className="parent-scissors">
                   <div className="child child-scissors"></div>
                 </div>
               </div>
             </div>
+            <div className="bottom">
+              <div className="bottom-center" onClick={() => { setHumanChoise('rock') }}>
+                <div className="parent-rock">
+                  <div className="child child-rock"></div>
+                </div>
+              </div>
+            </div>
           </main>
-          : <p>hard mode</p>}
+          // Hard mode
+          : <main className="hard-mode-container">
+            <div className="possition possition1" onClick={() => { setHumanChoise('scissors') }}>     <div className="parent-paper">
+              <div className="child child-paper"></div>
+            </div></div>
+            <div className="possition possition2">possition2</div>
+            <div className="possition possition3">possition3</div>
+            <div className="possition possition4">possition4</div>
+            <div className="possition possition5">possition5</div>
+            <div className="possition possition6">possition6</div>
+          </main>
+        }
       </div >
     </div >
   )
