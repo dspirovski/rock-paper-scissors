8a73ae8ec9667a685264d1eda8a775b0baabfb29
dspirovski <dspirovski@hotmail.com>
2022-01-21 13:40:51 +0100

    Fixing responsive layout

--- src/components/HardGameMode/HardGameMode.js
+++ src/components/HardGameMode/HardGameMode.js
@@ -17,41 +17,53 @@ function HardGameMode({ gameMode, setGameMode, score, setScore }) {
             <button className='mode-btn' onClick={() => setGameMode(false)}>hard</button>
           </div>
           <div className="hard-mode-body">
-            <motion.div
-              whileHover={{ scale: 1.03 }}
-              className="item scissors-position" onClick={() => { setHumanChoise(1) }}>
-              <div className="parent-scissors">
-                <div className="child child-scissors"></div>
-              </div>
-            </motion.div>
-            <motion.div
-              whileHover={{ scale: 1.03 }}
-              className="item paper-position" onClick={() => { setHumanChoise(2) }}>
-              <div className="parent-paper">
-                <div className="child child-paper"></div>
-              </div>
-            </motion.div>
-            <motion.div
-              whileHover={{ scale: 1.03 }}
-              className="item rock-position" onClick={() => { setHumanChoise(3) }}>
-              <div className="parent-rock">
-                <div className="child child-rock"></div>
-              </div>
-            </motion.div>
-            <motion.div
-              whileHover={{ scale: 1.03 }}
-              className="item lizard-position" onClick={() => { setHumanChoise(4) }}>
-              <div className="parent-lizard">
-                <div className="child child-lizard"></div>
-              </div>
-            </motion.div>
-            <motion.div
-              whileHover={{ scale: 1.03 }}
-              className="item spock-position" onClick={() => { setHumanChoise(5) }}>
-              <div className="parent-spock">
-                <div className="child child-spock"></div>
-              </div>
-            </motion.div>
+
+            {/* TOP position */}
+            <div className='position-top'>
+              <motion.div
+                whileHover={{ scale: 1.03 }}
+                className="scissors-position" onClick={() => { setHumanChoise(1) }}>
+                <div className="parent-scissors">
+                  <div className="child child-scissors"></div>
+                </div>
+              </motion.div>
+            </div>
+
+            {/* MIDDLE POSITION */}
+            <div className='position-middle'>
+              <motion.div
+                whileHover={{ scale: 1.03 }}
+                className="paper-position" onClick={() => { setHumanChoise(2) }}>
+                <div className="parent-paper">
+                  <div className="child child-paper"></div>
+                </div>
+              </motion.div>
+              <motion.div
+                whileHover={{ scale: 1.03 }}
+                className="rock-position" onClick={() => { setHumanChoise(3) }}>
+                <div className="parent-rock">
+                  <div className="child child-rock"></div>
+                </div>
+              </motion.div>
+            </div>
+
+            {/* position bottom */}
+            <div className='position-bottom'>
+              <motion.div
+                whileHover={{ scale: 1.03 }}
+                className="lizard-position" onClick={() => { setHumanChoise(4) }}>
+                <div className="parent-lizard">
+                  <div className="child child-lizard"></div>
+                </div>
+              </motion.div>
+              <motion.div
+                whileHover={{ scale: 1.03 }}
+                className="spock-position" onClick={() => { setHumanChoise(5) }}>
+                <div className="parent-spock">
+                  <div className="child child-spock"></div>
+                </div>
+              </motion.div>
+            </div>
           </div>
         </div>
         : <Game humanChoise={humanChoise} setHumanChoise={setHumanChoise} gameMode={gameMode} score={score} setScore={setScore} setGameMode={setGameMode} />}
